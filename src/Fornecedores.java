f43bf74823558c40e9fe8a1f30faf3472e904736
afranioag <afranio.garcia@ccc.ufcg.edu.br>
2018-10-25 11:00:35 -0300

    Mais classes

---
+++ src/Fornecedores.java
@@ -0,0 +1,68 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fornecedores {
+	private String nome;
+	private String email;
+	private String telefone;
+	private List <Produtos> produtos;
+	
+	public Fornecedores(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.produtos = new ArrayList();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fornecedores other = (Fornecedores) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return this.nome +" - "+this.email+" - "+this.telefone;
+	}
+
+}
