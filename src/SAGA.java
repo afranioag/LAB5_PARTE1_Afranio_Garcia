6c4bca647a7de4ff559cfd0c0a14145495473957
afranioag <afranio.garcia@ccc.ufcg.edu.br>
2018-10-25 11:15:48 -0300

    Class Saga adicionada]

---
+++ src/SAGA.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+
+public class SAGA {
+	private HashMap<String, Clientes> clientes = new HashMap<>();
+	private HashMap<String, Fornecedores> fornecedores = new HashMap<>();
+	
+}
